f1be3758742cd37042af13267a9bd78e6f31694f
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-02 14:08:24 -0500

    finsihed all videos on section components and databinding

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,9 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements =
+  [{type:'server', name:'Testserver', content:'This is jjust a test'}];
 
+  onServerAdded(serverData: {serverName:string, serverContent:string}) {
+     this.serverElements.push({
+     type: 'server',
+      name: serverData.serverName,
+     content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: {serverName:string, serverContent:string}) {
+     this.serverElements.push({
+       type: 'blueprint',
+     name: blueprintData.serverName,
+     content: blueprintData.serverContent
+ });
+
+  }
+  onChangeFirst() {
+    this.serverElements[0].name ='Changed';
+  }
 
 
 }
--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked,  } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+OnInit, OnChanges, DoCheck,
+AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked   {
 
-  constructor() { }
+ @Input('srvElement') element: {type:string, name:string, content:string};
+ @Input() name: string;
 
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called ');
   }
 
+  ngOnInit() {
+    console.log('ngOnInit called ');
+  }
+  ngOnChanges( ) {
+
+  }
+  ngDoCheck() {
+
+  }
+  ngAfterContentChecked() {
+
+  }
+  ngAfterContentInit() {
+
+  }
+  ngAfterViewChecked() {
+
+  }
+  ngAfterViewInit() {
+
+  }
 }
