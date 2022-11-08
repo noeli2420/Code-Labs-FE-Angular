bbeef4e5a9d51f2196de808be7a8acc2cc3f679e
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-08 14:21:20 -0600

    finished Using the Renderer

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -3,17 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHightlightDirective } from './better-highlights/better-hightlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighLightDirective
+    BasicHighLightDirective,
+    BetterHightlightDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
