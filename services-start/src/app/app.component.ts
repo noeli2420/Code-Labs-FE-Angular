351c33df48515a6dcdece82df43e880edfcdcd7c
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-15 13:15:48 -0600

    finisihed video how many instrances of service

--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 accounts:{name: string, status: string} [] = [];
 
 constructor(private accountsService: AccountsService) {}
