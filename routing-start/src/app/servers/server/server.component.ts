4cf1af3a5b1d20ce43b51609f269aee7ec293db4
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-29 14:21:55 -0600

    finished video "  Configuring the Handling of Query Parameters:

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -26,7 +26,8 @@ export class ServerComponent implements OnInit {
     );
   }
   onEdit() {
-    this.router.navigate(['edit'], {relativeTo:this.route});
+    this.router.navigate(['edit'],
+    {relativeTo:this.route, queryParamsHandling: 'preserve'});
   }
 
 }
