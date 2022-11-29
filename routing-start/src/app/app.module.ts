bdb7e0746bb87d247e162369fd95398d19ab2906
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-29 13:54:38 -0600

    finished video "  Practicing and some Common Gotchas"

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -17,7 +17,8 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent},
   { path: 'users/:id/:name', component: UsersComponent},
   { path: 'servers', component: ServersComponent},
-  { path: 'servers/:id/edit', component: ServersComponent}
+  { path: 'servers/:id', component: ServersComponent},
+  { path: 'servers/:id/edit', component: EditServerComponent}
 ];
 
 @NgModule({
