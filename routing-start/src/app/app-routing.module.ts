525c2d11b3d5a90da2e7e20ea2e480faeb8093c4
noeli2420 <cruznoeli24@g-apps.monett.k12.mo.us>
2022-11-30 13:13:07 -0600

    finished video "Protecting Routes with canActivate"

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AuthGuard } from "./auth-guard.service";
 
 
 const appRoutes: Routes = [
@@ -15,7 +16,7 @@ const appRoutes: Routes = [
   { path: 'users', component: UsersComponent, children: [
     { path: ':id/:name', component: UserComponent}
   ] },
-  { path: 'servers', component: ServersComponent, children: [
+  { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
     { path: ':id', component: ServerComponent},
     { path: ':id/edit', component: EditServerComponent}
   ]},
---
+++ routing-start/src/app/auth-guard.service.ts
@@ -0,0 +1,35 @@
+import { ActivatedRouteSnapshot,
+         CanActivate,
+         Router,
+         RouterStateSnapshot,
+} from "@angular/router";
+
+import { Observable } from "rxjs/Observable";
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot):Observable<boolean> | Promise<boolean> | boolean {
+  return this.authService.isAuthenticated()
+         .then(
+        (authenicated: boolean) => {
+          if (authenicated) {
+            return true;
+          }
+          else {
+            this.router.navigate (['/'])
+          }
+        }
+        );
+
+  }
+  }
+
+
+
+
